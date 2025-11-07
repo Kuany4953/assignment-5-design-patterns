351725b8b53e59348fb4b7607f2bddc85f5d41c3
Ibsa Tassew Geleta <82205002+ibsatassew@users.noreply.github.com>
2025-11-06 22:24:26 -0500

    Update HealCommand.java

--- src/main/java/edu/trincoll/game/command/HealCommand.java
+++ src/main/java/edu/trincoll/game/command/HealCommand.java
@@ -48,18 +48,7 @@ public class HealCommand implements GameCommand {
         this.amount = amount;
     }
 
-    @Override
-    public void execute() {
-        int before = target.getStats().health();
-        target.heal(amount);
-        int after = target.getStats().health();
-        actualHealingDone = after - before;
-    }
 
-    @Override
-    public void undo() {
-        target.setHealth(target.getStats().health() - actualHealingDone);
-    }
 
     @Override
     public String getDescription() {
